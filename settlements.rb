eceaa147b4687ca6588295bdb20efa4a69a7a560
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-12-11 15:52:47 -0500

    Create settlements.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ settlements.rb
@@ -0,0 +1,29 @@
+require "NeoPathfinding"
+
+puts "BASIC ELEMENTS"
+2.times do
+  get_statistics(:phantom_settlements,   "appear on maps but do not actually exist. Theories include copywrite traps, or suppression of evidence.",
+                 :phantom_islands,       "appear on the ocean as far back as early cartography, with disputed existance and verification. Reported by sailors, but later undiscovered.",
+                 :abandoned_attractions, "an abandoned amusement park that fell into disuse and became derelict, declared unsafe to travel.")
+        
+                 dynamic_reward_allocation
+end
+
+puts "\nFindings"
+
+2.times do
+  get_statistics(:abandoned_derelect_islands, "Had originally been planned to be part of theme park attractions, but were undiscovered when declared unsafe.",
+                 :theories,                   "Theories include haunted residents disliking modern architecture, to rides themselves being considered unsafe.",
+                 :mitigation,                 "Consider theme parks built on larger islands and Pennynsulas that aren't prone to sea level changes and restless spirits.")
+        
+                 dynamic_reward_allocation
+end
+
+puts "\nUpdates"
+2.times do
+  get_statistics(:related_concepts, "Phantom Settlements can also be secured using similar methods to containing threats in abandoned island theme parks.",
+                 :technology,       "Technology used should factor in flipped least priviledge that makes entities go through hoops to delivery information.",
+                 :other_strategies, "Other strategies include turning at risk islands into Pennynsulas in order to have easy escape routes to the mainland.")
+        
+                 dynamic_reward_allocation
+end
